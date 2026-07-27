423262a9a29fa1495d35cad1d11389493ca44ca4
Eissakarash <essakaresh@gmail.com>
2026-07-27 04:10:18 +0300

    Fix missing hero images

--- app/[lang]/(public)/(home)/@hero/page.tsx
+++ app/[lang]/(public)/(home)/@hero/page.tsx
@@ -1,13 +1,6 @@
 import React from "react";
 import Slider from "@/ui/molecules/carosul";
 import prisma from "@/lib/prisma";
-import HeroImage1 from "@/images/hero-villa-03.webp";
-import HeroImage2 from "@/images/hero-muheer-day.webp";
-import HeroImage3 from "@/images/hero-muheer-street.webp";
-import HeroImage4 from "@/images/hero-watad-evening.webp";
-import HeroImage5 from "@/images/hero-watad-night.webp";
-
-const fallbackImages = [HeroImage1, HeroImage2, HeroImage3, HeroImage4, HeroImage5];
 
 const HomePage = async () => {
   const values = await prisma.home.findFirst().catch((error) => {
@@ -15,7 +8,7 @@ const HomePage = async () => {
     return;
   });
 
-  const images = values?.images?.length ? values.images : fallbackImages;
+  const images = values?.images || [];
 
   return <Slider data={images} />;
 };
